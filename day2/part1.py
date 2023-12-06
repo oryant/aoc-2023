201a7ffc91242235c8238c9023ed272195abb2ca
oryant01 <tim@oryan.uk>
2023-12-06 12:59:56 +0000

    removed example

--- day2/part1.py
+++ day2/part1.py
@@ -1,8 +1,7 @@
 import os
 from typing import Tuple
 
-MAXIMA = {"red": 12, "green": 13, "blue": 14}
-example = "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
+LIMITS = {"red": 12, "green": 13, "blue": 14}
 
 
 def split_line(line: str) -> Tuple[int, list]:
@@ -19,8 +18,7 @@ with open(os.path.join(os.getcwd(), "day2/input.txt")) as input_file:
         splitline = split_line(line)
         valid_line = True
         for pull in splitline[1]:
-            if int(pull[0]) > MAXIMA[pull[1]]:
-                print(f"Bad game:{splitline[0]}, {pull[1]}, {pull[0]}")
+            if int(pull[0]) > LIMITS[pull[1]]:
                 valid_line = False
                 break
         sum_games += splitline[0] * valid_line
